36a6375290476fcf630642beb6a8ebae908ae43d
ik013043z1 <ik013043z1@H013043.NET>
2018-12-05 10:40:36 +0100

    2.0

--- src/EntornosPrueba.java
+++ src/EntornosPrueba.java
@@ -3,7 +3,6 @@ public class EntornosPrueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Jefry, Alberdi y Iosu");
-<<<<<<< HEAD
 		System.out.println("Hey there, I am using Whatsapp.");
 		System.out.println("Disponible");
 
