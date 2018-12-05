ade4a91c4cbd6ac57a35755ba4cb88cbfeffafc8
xalberdioy18dw <43572704+xalberdioy18dw@users.noreply.github.com>
2018-12-05 10:43:02 +0100

    Update EntornosPrueba.java

--- src/EntornosPrueba.java
+++ src/EntornosPrueba.java
@@ -5,7 +5,7 @@ public class EntornosPrueba {
 		System.out.println("Jefry, Alberdi y Iosu");
 		System.out.println("Hey there, I am using Whatsapp.");
 		System.out.println("Disponible");
-		System.out.println("WWW");
+		System.out.println("WWW, GG");
 	}
 	
 }
