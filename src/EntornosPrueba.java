2291f487b7a0dd216a913ff2eb2c46e98744e5b0
ik013043z1 <ik013043z1@H013043.NET>
2018-12-05 10:27:58 +0100

    1.2

--- src/EntornosPrueba.java
+++ src/EntornosPrueba.java
@@ -3,9 +3,9 @@ public class EntornosPrueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Jefry, Alberdi y Iosu");
-		System.out.println("Hey there, I am using Whatsapp.")
-		System.out.println("Disponible")
-		System.out.println("Sin bateria")
+		System.out.println("Hey there, I am using Whatsapp.");
+		System.out.println("Disponible");
+		System.out.println("Sin bateria");
 	}
 	
 }
